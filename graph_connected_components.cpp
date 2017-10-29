3fdc137f48b9c92e8a51b4adffe58d8fe190bca5
stark11 <30984117+stark11@users.noreply.github.com>
2017-10-29 14:46:47 +0530

    Create graph_connected_components.cpp

---
+++ graph_connected_components.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector <int> adj[10];
+bool visited[10];
+void dfs(int i)
+{
+    visited[i]=true;
+    for(int j=0;j<adj[i].size();j++)
+    {
+        if(visited[adj[i][j]]==false)
+            dfs(adj[i][j]);
+    }
+}
+int main()
+{
+    int nodes,edges,conncomp=0,x,y,i;
+    cin>>nodes>>edges;
+    for(i=0;i<edges;i++)
+    {
+        cin>>x>>y;
+        adj[x].push_back(y);
+
+    }
+    for(i=0;i<nodes;i++)
+        visited[i]=false;
+        for(i=1;i<=nodes;i++)
+        {
+            if(visited[i]==false)
+            {
+                dfs(i);
+                conncomp++;
+            }
+        }
+        cout<<"connected components"<<conncomp;
+        return 0;
+}
